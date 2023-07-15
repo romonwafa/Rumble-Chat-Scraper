fedd6096c1f170023a7442f9d85d1f8b0c6ac22b
romonwafa <romonwafa@gmail.com>
2023-07-15 07:18:14 -0400

    Release first version

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 # Rumble Live Chat Scraper
 
-Usage: `python3 scraper.py [URL]`
-
-This scraper is currently in beta and undergoing active development.
+Usage: `python3 scraper.py [options] [URL]`
 
 ## Current features:
 - Scrapes existing live chats from a live stream
+- Scrapes details associated with each chat message such as username, user id, message id, avatar url and profile url
+- Output csv file
 
 ## Roadmap
-- Continuously scrape new live chats as they come in
-- Add additional details for each message such as username, user id and message id
-- Add options
-	- Time to scrape for (in seconds)(0 for until end of stream)
-	- Include previous live chats in scrape (y/n)
-- More in future updates...
+- Listen for new chats and scrape them as they come in
+- Add option
+	- Time to scrape for (in seconds, or until end of stream)
--- scraper.py
+++ scraper.py
@@ -1,33 +1,94 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.options import Options 
-import requests
 from bs4 import BeautifulSoup
 from time import sleep
 import argparse
+import csv
 
+
+# Initialize arguments parser
 parser = argparse.ArgumentParser()
-parser.add_argument("URL", help="URL of a Rumble live stream")
+parser.add_argument('URL', help='URL of a Rumble live stream')
+parser.add_argument('-o', '--filename', nargs='?', default=None, help='output CSV file')
 args = parser.parse_args()
 
+# Initialize Selenium
 options = Options()
-options.add_argument("--headless")
+options.add_argument('--headless')
 driver = webdriver.Firefox(options=options)
 driver.get(args.URL)
 
+# Wait for chats to load
+print("Scraping live chats...\n")
 sleep(3)
 
-# V1.0 (beta)
-# Currently only scrapes historical messages
-
-# TODO: 
-# Add live comment scraping
-# Add additional args (time to scrape for?, include historical comments?)
+# Nest message lists into one big list
+messages_list = []
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
+chat_history = soup.find('div', class_='chat-history')
+try:
+	messages = chat_history.find_all('li', class_='chat-history--row')
+except AttributeError:
+	print('Channel is not live.')
+	driver.quit()
+	quit()
 
-messages = soup.find_all('div', class_="chat-history--message")
+# Quit driver
+driver.quit()
 
+# Scrape messages
 for message in messages:
-	print(message.get_text())
+	# True when message is a rant message
+	is_rant = False
+	# Create list to store information about each message
+	message_data = []
+
+	# Get user id
+	userid = message['data-message-user-id']
+	# Get message id
+	messageid = message['data-message-id']
+	# Get user avatar (TypeError exception occurs if user has no avatar image)
+	try:
+		avatar = message.find('img', class_='chat-history--user-avatar')
+		avatar_url = avatar['src']
+	except TypeError:
+		avatar_url = ''
+	
+	# Note! AttributeError exceptions occur on rant messages (they don't)
+	# TODO: Handle rant chats properly
+	try:
+		# Get message wrapper
+		# Need to handle this properly...
+		message_wrapper = message.find('div', class_='chat-history--message-wrapper')
+			
+		# Get username
+		username_div = message_wrapper.find('div', class_='chat-history--username')
+		username_hyperlink = username_div.find('a')
+		username = username_hyperlink.get_text()
+		# Get profile URL
+		profile_url = username_hyperlink['href']
+		# Get message text
+		message_text = message_wrapper.find('div', class_='chat-history--message').get_text()
+
+		# Append message information to a list
+		message_data.append(userid)
+		message_data.append(messageid)
+		message_data.append(avatar_url)
+		message_data.append(profile_url)
+		message_data.append(username)
+		message_data.append(message_text)
+		print(message_data)
+		# Append message_data to messages_list
+		messages_list.append(message_data)
+	except AttributeError:
+		pass
+
+# Write .csv file
+if args.filename is not None:
+	fields = ['User ID', 'Message ID', 'Avatar URL', 'Profile URL', 'Username', 'Message Text']
+	with open(args.filename, 'w') as f:
+		write = csv.writer(f)
+		write.writerow(fields)
+		write.writerows(messages_list)
+	print(f"\nExported {args.filename}\n")
